238d05b4a6199080f1763f0ebeb68aee98ef3103
hongxiang <gohuhx@163.com>
2023-02-18 18:25:08 +0800

    [huhx] -feat: export dart files

--- lib/app_common_flutter.dart
+++ lib/app_common_flutter.dart
@@ -1,7 +1,28 @@
 library app_common_flutter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// extension
+export 'src/common/extension/context_extension.dart';
+export 'src/common/extension/date_extension.dart';
+export 'src/common/extension/list_extension.dart';
+export 'src/common/extension/string_extension.dart';
+
+// util
+export 'src/common/util/app_config.dart';
+export 'src/common/util/comm_util.dart';
+
+// components
+export 'src/component/appbar_back_button.dart';
+export 'src/component/center_progress_indicator.dart';
+export 'src/component/custom_load_footer.dart';
+export 'src/component/custom_water_drop_header.dart';
+export 'src/component/empty_widget.dart';
+export 'src/component/list_scroll_physics.dart';
+export 'src/component/list_tile_item.dart';
+export 'src/component/list_tile_trailing.dart';
+export 'src/component/svg_action_icon.dart';
+export 'src/component/svg_icon.dart';
+export 'src/component/text_icon.dart';
+
+// pagination
+export 'src/pagination/paged_view.dart';
+export 'src/pagination/stream_state.dart';
