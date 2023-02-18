b3917b78df23192b13d667a34290b5800148988d
hongxiang <gohuhx@163.com>
2023-02-18 16:46:46 +0800

    [huhx] -feat: init project and add dio dependencies

---
+++ lib/src/common/client/dio_client.dart
@@ -0,0 +1,10 @@
+import 'package:dio/dio.dart';
+
+class RestClient {
+  static Dio getInstance() {
+    return Dio(BaseOptions())
+      ..interceptors.add(
+        LogInterceptor(responseBody: true, requestBody: true),
+      );
+  }
+}
