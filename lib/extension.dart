dbf56726b2143e8b2e0e6cc22257a990167df8c0
hongxiang <gohuhx@163.com>
2023-02-19 10:14:16 +0800

    [huhx] -feat: add int extension

--- lib/extension.dart
+++ lib/extension.dart
@@ -2,3 +2,4 @@ export 'src/common/extension/context_extension.dart';
 export 'src/common/extension/date_extension.dart';
 export 'src/common/extension/list_extension.dart';
 export 'src/common/extension/string_extension.dart';
+export 'src/common/extension/int_extension.dart';
---
+++ lib/src/common/extension/int_extension.dart
@@ -0,0 +1,29 @@
+import 'package:app_common_flutter/constant.dart';
+import 'package:intl/intl.dart';
+
+extension IntExtension on int {
+  String get dateString {
+    return format(Comm.datePattern);
+  }
+
+  String get dateTimeString {
+    return format(Comm.dateTimePattern);
+  }
+
+  String format(String pattern) {
+    return DateFormat(pattern)
+        .format(DateTime.fromMillisecondsSinceEpoch(this));
+  }
+
+  String get timeString {
+    if (this <= 0) {
+      return "00:00";
+    }
+    final int second = this % 60;
+    final int minute = (this / 60).floor();
+
+    final String secondString = second < 10 ? "0$second" : "$second";
+    final String minuteString = minute < 10 ? "0$minute" : "$minute";
+    return "$minuteString:$secondString";
+  }
+}
