8980d10e50ea2afe7f986922a14a1aba59f8eed1
hongxiang <gohuhx@163.com>
2023-05-31 15:54:05 +0800

    [huhx] -feat: rename to _logger

--- lib/src/common/logger/log.dart
+++ lib/src/common/logger/log.dart
@@ -1,35 +1,31 @@
 import 'package:logger/logger.dart';
 
 class Log {
-  static final Logger logger = Logger(
+  static final Logger _logger = Logger(
     printer: PrettyPrinter(),
   );
 
-  static void setLevel(Level level) {
-    Logger.level = level;
-  }
-
   static void v(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    logger.v(message, error, stackTrace);
+    _logger.v(message, error, stackTrace);
   }
 
   static void d(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    logger.d(message, error, stackTrace);
+    _logger.d(message, error, stackTrace);
   }
 
   static void i(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    logger.i(message, error, stackTrace);
+    _logger.i(message, error, stackTrace);
   }
 
   static void w(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    logger.w(message, error, stackTrace);
+    _logger.w(message, error, stackTrace);
   }
 
   static void e(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    logger.e(message, error, stackTrace);
+    _logger.e(message, error, stackTrace);
   }
 
   static void log(Level level, dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    logger.log(level, message, error, stackTrace);
+    _logger.log(level, message, error, stackTrace);
   }
 }
