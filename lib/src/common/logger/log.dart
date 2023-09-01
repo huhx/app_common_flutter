fb0d015fdb0ab78f4352da059d0a8f2bfbc31d73
hongxiang <gohuhx@163.com>
2023-09-01 09:23:25 +0800

    [huhx] -feat: upgrade the logger version

--- lib/src/common/logger/log.dart
+++ lib/src/common/logger/log.dart
@@ -6,26 +6,26 @@ class Log {
   );
 
   static void v(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.v(message, error, stackTrace);
+    _logger.t(message, error: error, stackTrace: stackTrace);
   }
 
   static void d(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.d(message, error, stackTrace);
+    _logger.d(message, error: error, stackTrace: stackTrace);
   }
 
   static void i(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.i(message, error, stackTrace);
+    _logger.i(message, error: error, stackTrace: stackTrace);
   }
 
   static void w(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.w(message, error, stackTrace);
+    _logger.w(message, error: error, stackTrace: stackTrace);
   }
 
   static void e(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.e(message, error, stackTrace);
+    _logger.e(message, error: error, stackTrace: stackTrace);
   }
 
   static void log(Level level, dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.log(level, message, error, stackTrace);
+    _logger.log(level, message, error: error, stackTrace: stackTrace);
   }
 }
